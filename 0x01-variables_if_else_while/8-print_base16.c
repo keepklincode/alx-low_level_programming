71ede697fffdccd9d34ad832ba3261443603aee8
keepklincode <keepklincode@gmail.com>
2023-07-16 12:22:04 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: "Print base 16"
+* Return: Always return 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 103; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
