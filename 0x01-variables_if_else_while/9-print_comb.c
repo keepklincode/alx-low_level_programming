1fa4ea4b8c7385fdb3075e28349a9cb26a250269
keepklincode <keepklincode@gmail.com>
2023-07-16 12:38:07 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Description: "Print base 16"
+* Description: "Print combo"
 * Return: Always return 0
 */
 int main(void)
 {
 	int n;
-	int m;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-	}
-	for (m = 97; m <= 102; m++)
-	{
-		putchar(m);
+		putchar(',');
+		putchar(' ');
+
 	}
 	putchar('\n');
 	return (0);
