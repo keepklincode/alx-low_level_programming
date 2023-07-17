6868c5d95d20e9812c254964e9bd38d781398164
keepklincode <keepklincode@gmail.com>
2023-07-17 13:23:49 -0700

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int d, p;
 
-	for (d = 0; d < 9; d++)
+	for (d = '0'; d < '9'; d++)
 	{
-	for (p = 1; p <= 9; p++)
+	for (p = '1'; p <= '9'; p++)
 	{
 	if (p != d)
 	{
 	putchar(d);
 	putchar(p);
-	if (d == 8 && p == 9)
+	if (d == '8' && p == '9')
 	continue;
 	putchar(',');
 	putchar(' ');
