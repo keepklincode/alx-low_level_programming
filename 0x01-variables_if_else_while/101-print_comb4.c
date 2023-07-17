c4861285327df830a8dd6dfcf2fbadb190d8d7db
keepklincode <keepklincode@gmail.com>
2023-07-17 13:51:42 -0700

    prin-com

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: "Print combo4"
+* Return: Always return 0
+*/
+int main(void)
+{
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
+	for (q = p + 1; 1 <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
