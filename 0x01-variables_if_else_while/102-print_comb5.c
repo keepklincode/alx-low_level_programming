7ace176a6641104d68ae5b1ad62556252d3ade6f
keepklincode <keepklincode@gmail.com>
2023-07-17 14:08:48 -0700

    print-comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: "Print numbes"
+* Return: Always return 0
+*/
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 9; p++)
+	{
+	for (q = p + 1; q <= 9; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 990)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
