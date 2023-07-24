cbce7e469017afc9bb7dc213889e1bd0539294ab
keepklincode <keepklincode@gmail.com>
2023-07-24 16:30:54 -0700

    main

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,4 @@
-#include "main-h"
+#include "main.h"
 
 /**
 * _strcpy - Copy a string
@@ -7,7 +7,7 @@
 * Return: the pointer to dest
 */
 
-char _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i;
 for (i = 0; src[i] != '\0'; i++)
