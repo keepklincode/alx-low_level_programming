f49917953ba29115b8d6fb07be23448c4fb3e619
keepklincode <keepklincode@gmail.com>
2023-07-24 14:03:27 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* reset_to_98 - Takes a pointer to an int and updatea
+* the value it points to is 98
+*
+* @n: An integet input pointer
+*
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
